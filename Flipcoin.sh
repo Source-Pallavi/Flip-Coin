337f3f69c7a0c75c3c4151bc6808fef935d28155
Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-07-29 09:26:53 +0530

    Update Flipcoin.sh

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -1,89 +1,85 @@
-!/bin/bash -x
-echo "Flip coin Simulator"
-echo "Enter the count u want to flip the coin"
+#!/bin/bash
+
+echo "flip Coin Simulator"
+
+echo "number of times user want to flip the coin"
 read flip
-hh=0
-tt=0
-th=0
-ht=0
+echo $flip
+
+countHHH=0
+countTTT=0
+countTHT=0
+countHTH=0
+countHHT=0
+countTTH=0
+countTHH=0
+countHTT=0
 declare -A noOfCounts
 for((i=0;i<flip;i++))
 do
-rm=$((RANDOM%2))
-if [ $rm -eq 0 ]
+if [ $((RANDOM%8)) -eq 0 ]
 then
-((hh++))
-elif [ $rm -eq 1 ]
+((countHHH++))
+elif [ $((RANDOM%8)) -eq 1 ]
+then
+((countTTT++))
+elif [ $((RANDOM%8)) -eq 2 ]
+then
+((countTHT++))
+elif [ $((RANDOM%8)) -eq 3 ]
+then
+((counHHT++))
+elif [ $((RANDOM%8)) -eq 4 ]
+then
+((countTHH++))
+elif [ $((RANDOM%8)) -eq 5 ]
+then
+((countHTT++))
+elif [ $((RANDOM%8)) -eq 6 ]
 then 
-((tt++))
-elif [ $rm -eq 2 ]
-((th++))
-else 
-((ht++))
+((countHTH++))
+else
+((countTTH++))
 fi
 done
-echo "head-head count ="$hh
-echo "tail-tail count ="$tt
-echo "head-tail count ="$ht
-echo "tail-head count ="$th
-echo "Percentage of Tails-tails= "$((100*$tt/$flip))
-echo "Percentage of Heads-heads= "$((100*$hh/$flip))
-echo "Percentage of Tails-tails= "$((100*$th/$flip))
-echo "Percentage of Heads-heads= "$((100*$th/$flip))
-echo "For tripplet "
-hhh=0
-ttt=0
-tth=0
-tht=0
-thh=0
-hht=0
-htt=0
-hth=0
-declare -A noOfCounts
-for((i=0;i<flip;i++))
-do
-rm=$((RANDOM%9))
-if [ $rm -eq 0 ]
-then
-((hhh++))
-elif [ $rm -eq 1 ]
-then 
-((ttt++))
-elif [ $rm -eq 2 ]
-then
-((tth++))
-elif [ $rm -eq 3 ]
-then 
-((tht++))
-elif [ $rm -eq 4 ]
-((thh++))
-elif [ $rm -eq 5 ]
-then 
-((thh++))
-elif [ $rm -eq 6 ]
-then
-((htt++))
-else 
-((tht++))
-fi
-done
-echo "head-head-head count ="$hhh
-echo "tail-tail-tail count ="$ttt
-echo "head-tail-tail count ="$htt
-echo "tail-head-tail count ="$tht
-echo "head-tail-head count ="$hth
-echo "head-head-tail count ="$hht
-echo "head-tail-head count ="$hth
-echo "tail-head-head count ="$thh
-echo "Percentage of Tails-tails= "$((100*$ttt/$flip))
-echo "Percentage of head-head-head= "$((100*$hhh/$flip))
-echo "Percentage of head-tail-tail= "$((100*$htt/$flip))
-echo "Percentage of tail-head-tail= "$((100*$hth/$flip))
-echo "Percentage ofhead-tail-head= "$((100*$hth/$flip))
-echo "Percentage of head-head-tail= "$((100*$hht/$flip))
-echo "Percentage of head-tail-head= "$((100*$hth/$flip))
-echo "Percentage of tail-head-head= "$((100*$thh/$flip))
-function winning(){
+echo "HHH="$countHHH
+echo "TTT="$countTTT
+echo "THT="$countTHT
+echo "HTH="$countHTH
+echo "HHT="$countHHT
+echo "TTH="$countTTH
+echo "THH="$countTHH
+echo "HTT="$countHTT
+
+noOfCounts[hhh]=$countHHH
+noOfCounts[ttt]=$countTTT
+noOfCounts[tht]=$countTHT
+noOfCounts[hth]=$countHTH
+noOfCounts[hht]=$countHHT
+noOfCounts[tth]=$countTTH
+noOfCounts[thh]=$countTHH
+noOfCounts[htt]=$countHTT
+
+echo ${noOfCounts[@]}
+
+PercentageOfTTT=$((100*$countTTT/$flip))
+echo "Percentage of TTT= "$PercentageOfTTT
+PercentageOfHHH=$((100*$countHHH/$flip))
+echo "Percentage of HHH= "$PercentageOfHHH
+PercentageOfTHT=$((100*$countTHT/$flip))
+echo "Percentage of THT= "$PercentageOfTHT
+PercentageOfHTH=$((100*$countHTH/$flip))
+echo "Percentage of HTH= "$PercentageOfHTH
+PercentageOfHHT=$((100*$countHHT/$flip))
+echo "Percentage of HHT= "$PercentageOfHHT
+PercentageOfTHH=$((100*$countTHH/$flip))
+echo "Percentage of THH= "$PercentageOfTHH
+PercentageOfHTT=$((100*$countHTT/$flip))
+echo "Percentage of HTT= "$PercentageOfHTT
+PercentageOfTTH=$((100*$countTTH/$flip))
+echo "Percentage of TTH= "$PercentageOfTTH
+
+function wining(){
 
 array=($(for size in ${noOfCounts[@]}
 do
@@ -103,7 +99,4 @@ do
 done
 }
 
-winning
-
-
-
+wining
